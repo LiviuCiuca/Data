07a3921aeda8ba5fe62515ed69cd1db76c93a4f5
Liviu Ciuca <lc874@canterbury.ac.uk>
2023-04-29 12:37:30 +0000

    changed the executeSQLQueries() function to display the results in a table format for estetics, more on this in the doc

--- src/functions.php
+++ src/functions.php
@@ -102,19 +102,38 @@ function executeMySQLQueries($queries) {
         $result = $conn->query($query);
 
         if ($result) {
-            echo "Query executed: $query\n";
+            echo "<h3>Query executed: $query</h3>\n";
             if ($result->num_rows > 0) {
+                // Print the data in an HTML table
+                echo "<table border='1' cellpadding='5'>\n";
+                $headerPrinted = false;
                 while($row = $result->fetch_assoc()) {
-                    print_r($row);
+                    // Print the table headers
+                    if (!$headerPrinted) {
+                        echo "<tr>";
+                        foreach ($row as $key => $value) {
+                            echo "<th>" . htmlspecialchars($key) . "</th>";
+                        }
+                        echo "</tr>\n";
+                        $headerPrinted = true;
+                    }
+                    // Print the table data
+                    echo "<tr>";
+                    foreach ($row as $value) {
+                        echo "<td>" . htmlspecialchars($value) . "</td>";
+                    }
+                    echo "</tr>\n";
                 }
+                echo "</table>\n";
             } else {
-                echo "0 results.\n";
+                echo "0 results.<br>\n";
             }
         } else {
-            echo "Error executing query: " . $conn->error;
+            echo "Error executing query: " . $conn->error . "<br>\n";
         }
     }
 
     $conn->close();
 }
+
 ?>
